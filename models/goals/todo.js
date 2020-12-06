3587cbf0bddc08cc82ab714e55eb9f1c46723cf8
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-12-06 10:36:48 +0100

    toto model

---
+++ models/goals/todo.js
@@ -0,0 +1,47 @@
+
+const { v4: uuid } = require('uuid')
+
+module.exports = ({ sequelize, Sequelize }) => {
+
+    const ToDo = sequelize.define("todos", {
+        id: {
+            primaryKey: true,
+            type: Sequelize.UUID,
+            default: Sequelize.UUIDV4
+        },
+        subject: {
+            type: Sequelize.STRING,
+        },
+        done:{
+            type:Sequelize.BOOLEAN
+        },
+        startDate: {
+            type: Sequelize.DATE,
+        },
+        endDate: {
+            type: Sequelize.DATE,
+        },
+        descriptions: {
+            type: Sequelize.STRING,
+        },
+        isActive: {
+            type: Sequelize.BOOLEAN,
+            default: true
+        },
+        // Timestamps
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE,
+    }, {
+        indexes: [
+            {
+                fields: ['id', 'userId']
+            }
+        ]
+    });
+    ToDo.associate = (models) => {
+        ToDo.belongsTo(models.User, { as: 'user', foreignKey: 'userId' })
+    }
+
+    ToDo.beforeCreate(todo => todo.id = uuid())
+    return ToDo;
+};
