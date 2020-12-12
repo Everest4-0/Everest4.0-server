2825aafe5e7dbff6ad8ffc0da0c0e88ee97a44da
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-12-12 09:06:45 +0100

    gdiag controller

---
+++ controllers/diagnostics/evaluation-request.controller.js
@@ -0,0 +1,110 @@
+const Email = require('../../application/mail/mail');
+var { EvaluationRequest, Evaluation, User,  UserEvaluation } = require('../../models/models');
+
+exports.create = async (req, res) => {
+    req.body.requesterId = req.body.requester.id
+    req.body.requestedId = req.body.requested.id
+    if(req.body.requestedId===undefined){
+//        let requested=await User.create({email:req.body.requested.email})
+        let requested=await User.findAll({where:{email:req.body.requested.email}})
+        req.body.requestedId=requested.id;
+        let email=new Email({
+            to:req.body.requested.email,
+            subject:'Evaluation Request',
+            template:'evaluation_request',
+            data:{
+                from:req.body.requester,
+                to:requested
+            }
+        })
+        email.send((e,s)=>{
+
+            let t=e
+        })
+    }
+
+    let evaluations = await EvaluationRequest.create(req.body).catch((e, r) => {
+        let h = e;
+    });
+    res.json(evaluations)
+}
+
+exports.update = async (req, res) => {
+
+    let evaluations = EvaluationRequest.update({ lastName: "Doe" }, {
+        where: {
+            lastName: null
+        }
+    });
+    res.json({
+        status: 200,
+        message: "success",
+        data: evaluations
+    });
+}
+
+exports.delete = async (req, res) => {
+    let evaluations = EvaluationRequest.destroy({})
+    res.json({
+        status: 200,
+        message: "sucess",
+        data: evaluations
+    });
+}
+
+exports.one = async (req, res) => {
+
+    let evaluations = await EvaluationRequest.findOne();
+    res.json({
+        status: 200,
+        message: "sucess",
+        data: evaluations
+    })
+
+}
+
+exports.allBy = async (req, res) => {
+
+    /*
+     let filter = {}
+ 
+     if (req.query['$filter']) {
+         filter=[
+             {requestedId: req.query['$filter']},
+             {requesterId: req.query['$filter']}
+         ]
+     }*/
+
+
+    let evaluations = await EvaluationRequest.findAll({
+        where: req.query,
+        include: [
+            {
+                model: User,
+                as: 'requester'
+            },
+            {
+                model: User,
+                as: 'requested'
+            },
+            {
+                model: UserEvaluation,
+                as: 'evaluations',
+                include: [
+
+                    {
+                        model: Evaluation,
+                        as: 'evaluation'
+                    },
+                    {
+                        model: EvaluationRequest,
+                        as: 'request'
+                    }
+                ]
+            }
+        ]
+    }).catch((e, r) => {
+        let u = e
+    });
+    res.json(evaluations)
+}
---
+++ controllers/diagnostics/user-evaluation.controller.js
@@ -0,0 +1,79 @@
+var {UserEvaluation, User, Evaluation, EvaluationRequest} = require('../../models/models');
+
+exports.create = async (req, res) => {
+
+    req.body.userId=req.body.requester.id
+    req.body.requestedId=req.body.requested.id
+    req.body.evaluationId=req.body.evaluation.id
+    req.body.requestId=req.body.request.id
+    let userEvaluations = await UserEvaluation.create(req.body);
+    res.json(userEvaluations)
+}
+
+exports.update = async (req, res) => {
+
+    let userEvaluations = UserEvaluation.update({ lastName: "Doe" }, {
+        where: {
+            lastName: null
+        }
+    });
+    res.json({
+        status: 200,
+        message: "success",
+        data: userEvaluations
+    });
+}
+
+exports.delete = async (req, res) => {
+    let userEvaluations = UserEvaluation.destroy({})
+    res.json({
+        status: 200,
+        message: "sucess",
+        data: userEvaluations
+    });
+}
+
+exports.one = async (req, res) => {
+
+    let userEvaluations = await UserEvaluation.findOne();
+    res.json({
+        status: 200,
+        message: "sucess",
+        data: userEvaluations
+    })
+
+}
+
+exports.allBy = async (req, res) => {
+
+    let t;
+    /* UserEvaluation.consts.forEach(async element => {
+      t=   await UserEvaluation.create(element).catch(e=>{
+        let i=e;
+    })
+     });*/
+    let userEvaluations = await UserEvaluation.findAll({
+        where:req.query,
+        include: [
+            {
+                model: User,
+                as: 'requester'
+            },
+            {
+                model: User,
+                as: 'requested'
+            },
+            {
+                model: Evaluation,
+                as: 'evaluation'
+            },
+            {
+                model: EvaluationRequest,
+                as: 'request'
+            }
+        ]
+    })
+
+    
+    res.json(userEvaluations)
+}
