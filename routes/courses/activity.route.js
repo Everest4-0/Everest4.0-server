f5c0dc0974d6a51db91102d87987c7a7a0b9c972
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-01-09 15:36:24 +0100

    courses router fix

--- routes/courses/activity.route.js
+++ routes/courses/activity.route.js
@@ -4,10 +4,12 @@ let controller = require('../../controllers/courses/activity.controller');
 // Export API routes
 module.exports = router;
 router.get('/', controller.allBy);
-router.get('/:id', controller.one);
 router.post('/', controller.create);
 router.put('/', controller.update);
 router.delete('/', controller.delete);
+router.post('/add_user_answer', controller.addUserAnswer);
+router.get('/user_answer', controller.getUserAnswer);
+router.get('/:id', controller.one);
 
 // Export API routes
 module.exports = router;
