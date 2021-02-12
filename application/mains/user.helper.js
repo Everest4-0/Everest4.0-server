6ab28e314b03699365889d417d13a1304de2a165
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-02-12 22:06:50 +0100

    fixed

---
+++ application/mains/user.helper.js
@@ -0,0 +1,35 @@
+const { User } = require("../../models/models")
+
+const jwt = require('jsonwebtoken')
+
+const serverJWT_secret = "We Have To Set A Strong Secret And Store It On A Secure Place"
+class UserHelper {
+
+    static currentUserAuthToken = async (key) => await User.find({ where: { email: key } });
+
+    static generateUserAuthToken = (user) => 'Bearer ' + jwt.sign(user.email, serverJWT_secret)
+
+    static validateUserAuthToken = (req, res, next) => {
+        let token = req.headers['authorization']
+        if (typeof token !== 'string' && token.split(' ').length < 2) {
+            res.sendStatus(403)
+        }
+        jwt.verify(token.split(' ')[1], serverJWT_secret, (err, decoded) => {
+            if (err) {
+                res.sendStatus(403)
+            } else {
+                req.user = UserHelper.currentUserAuthToken(decoded)
+                next()
+            }
+        })
+    }
+
+    static setUserAuthToken = (req, res) => {
+        let user = req.user
+        user.apikey = UserHelper.generateUserAuthToken(user)
+        res.send(req.user)
+    }
+
+}
+
+module.exports = UserHelper;
--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 module.exports = {
   dev: {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "everest_dev_2",
+    USER: "dev",
+    PASSWORD: "qasw",
+    DB: "everest_dev",
     dialect: "mysql",
     pool: {
       max: 5,
@@ -20,6 +20,26 @@ module.exports = {
     USER: "qa@everest40db",
     PASSWORD: "2wsxZAQ!",
     DB: "everest_qa",
+    dialect: "mysql",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        ca: fs.readFileSync(__dirname + '/../server/ssl/crt.pem')
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  },
+  qa2: {
+    HOST: "185.247.119.207",
+    USER: "qauser",
+    PASSWORD: "2SXzaQQu;",
+    DB: "everest_qa2",
     dialect: "mysql",
 
     dialectOptions: {
