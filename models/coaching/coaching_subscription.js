b6e997f783f698a1e0cce49c85a83fb07c60ef4d
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-03-14 13:04:48 +0100

    refactor socahing subscription

--- models/coaching/coaching_subscription.js
+++ models/coaching/coaching_subscription.js
@@ -1,41 +1,41 @@
-const {v4:uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 
 
-module.exports = ({sequelize, Sequelize}) => {
-    const CoachingSubscription = sequelize.define('coaching_subscription', {
-        id:{
+module.exports = ({ sequelize, Sequelize }) => {
+    const CoachingSubscription = sequelize.define('coaching_subscriptions', {
+        id: {
             primaryKey: true,
             type: Sequelize.UUID,
             default: Sequelize.UUIDV4
         },
-        description:{
+        description: {
             type: Sequelize.STRING
         },
-        isActive:{
-            type:Sequelize.BOOLEAN,
+        isActive: {
+            type: Sequelize.BOOLEAN,
             default: true
         },
         //Timestamp
+        activatedAt: Sequelize.DATE,
         createdAt: Sequelize.DATE,
         updatedAt: Sequelize.DATE
     }, {
-        indexes:[{
-            fields:['id', 'userId', 'durationId', 'goalId']
+        indexes: [{
+            fields: ['id', 'userId']
         }]
     });
 
-    CoachingSubscription.associate = (models) =>{
+    CoachingSubscription.associate = (models) => {
 
-        CoachingSubscription.hasMany(models.CoachingDuration);
-        CoachingSubscription.hasMany(models.CoachingGoal);
+        CoachingSubscription.belongsTo(models.CoachingGoal, { as: 'goal', foreignKey: 'goalId' });
+        CoachingSubscription.belongsTo(models.CoachingDuration, { as: 'duration', foreignKey: 'durationId' });
+        CoachingSubscription.belongsTo(models.User, {as: 'user',foreignKey: 'userId'})
+        CoachingSubscription.belongsTo(models.User, {as: 'coach',foreignKey: 'coachId'})
+        CoachingSubscription.belongsTo(models.Chat, {as: 'chat',foreignKey: 'chatId'})
 
-        CoachingSubscription.belongsTo(models.User, {
-            as: 'user',
-            foreignKey: 'userId'
-        })
     }
 
     CoachingSubscription.beforeCreate(coachingSubscribe => coachingSubscribe.id = uuid());
-    
+
     return CoachingSubscription;
 }
