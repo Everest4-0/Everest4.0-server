7642cac0110fb14e752b3f9f35c4f8c8b64f74dc
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-03-20 21:23:52 +0100

    feedbacks fro coaching controller and routes

--- routes/route.js
+++ routes/route.js
@@ -36,6 +36,7 @@ router.use('/coaching/notes', require("./coaching/note.route"));
 router.use('/coaching/subscriptions', require("./coaching/coaching_subscription.route"));
 router.use('/coaching/durations', require("./coaching/coaching_duration.route"));
 router.use('/coaching/goals', require("./coaching/coaching_goal.route"));
+router.use('/coaching/feedbacks', require("./coaching/feedback.route"));
 //router.use('/answers', require("./quiz/answer.route"));
 
 // Export API routes
