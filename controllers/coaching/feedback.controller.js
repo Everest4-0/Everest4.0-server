1ff5d67d6605355ac0197d54454ad9246aa57628
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-03-21 00:03:23 +0100

    feedbacks items fro coaching controller and routes

--- controllers/coaching/feedback.controller.js
+++ controllers/coaching/feedback.controller.js
@@ -7,6 +7,14 @@ exports.create = async (req, res) => {
     let feedback = await Feedback.create(req.body).catch((e, feedback) => {
         res.status(400).json(e || feedback)
     });
+
+    req.body.points.forEach(async point=>{
+        point.feedbackId=feedback.id
+        point.itemId=point.item.id
+        await FeedbackPoint.create(point).catch((e, feedback) => {
+            let err=e
+        })
+    })
     res.json(feedback)
 }
 
@@ -58,10 +66,14 @@ exports.one = async (req, res) => {
 
 exports.allBy = async (req, res) => {
 
-    let filter = {...req.query,...{ userId: req.user.id }}
+    let filter = {...req.query,...{  }}
 
     let feedbacks = await Feedback.findAll({
         where: filter,
+        order: [
+            // Will escape title and validate DESC against a list of valid direction parameters
+            ['createdAt', 'ASC']
+        ],
         include: [
             {
                 model: User,
