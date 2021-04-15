db39321a7368f618b81b4a4ed39a33ec8a85598b
PedroSousa2020 <sousapedro747@gmail.com>
2021-04-15 23:05:53 +0000

    Charge.route.js created

---
+++ routes/payment/stripe/charge.route.js
@@ -0,0 +1,11 @@
+var router = require('express').Router();
+let controller = require('../../../controllers/payment/stripe/charge.controller');
+
+//router.get('/', controller.allBy);
+//router.get('/:id', controller.one);
+router.post('/', controller.create);
+//router.put('/', controller.update);
+//router.delete('/:id', controller.delete);
+
+// Export API routes
+module.exports = router;
