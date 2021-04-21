af54b7fb52cbaf698a54025680382204937de591
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-04-21 18:56:23 +0100

    charge controller

--- controllers/payment/charge.controller.js
+++ controllers/payment/charge.controller.js
@@ -1,7 +1,8 @@
 const { Address, Customer, Charge } = require('../../models/models');
-
+const moment = require('moment');
 const Stripe = require('stripe');
 const ChargeApplication = require('../../application/payment/charge.application');
+const referenceApplication = require('../../application/payment/reference.application');
 
 const stripe = Stripe('sk_test_51IeHexKPeIS2foQk5ur9bFa32oT3Q9Yf0DdShqfhRgy1Yo8DC2jJuhN2iKPzpNSXik4G8OtJgsJTvp8w5Yd47jS800UZItfE06');
 
@@ -18,7 +19,7 @@ exports.create = async (req, res) => {
 
     let ourCustomer = { id: null };
 
-    let callback = async (charge) => {
+    let callback = async (charge, callback) => {
         let ourCharge = await Charge.create({
             descriptions: charge.descriptions,
             amount: amount,
@@ -28,13 +29,23 @@ exports.create = async (req, res) => {
         })
         ourCharge.customer = ourCustomer;
         ourCharge.quantity = req.body.quantity
+
+        let u = callback(ourCharge)
         res.json(ourCharge)
 
     }
 
     if (req.body.type === ChargeApplication.REFERENCE) {
         req.body.isActive = false
-        callback(req.body);
+        callback(req.body, async (data) => {
+            return  final = await referenceApplication.create({
+                amount: data.amount,
+                end_datetime: moment(data.createdAt).add(1, 'days').format('YYYY-MM-DD'),
+                custom_fields: {
+                    reference: data.reference.toString()
+                }
+            })
+        });
     } else if (req.body.type === ChargeApplication.CREDITCARD) {
         req.body.isActive = true;
         stripe.customers.create(req.body.customer)
