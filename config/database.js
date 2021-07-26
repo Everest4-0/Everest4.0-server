dfeeaa14c92b8856d1e016d23deb82f33e4aeeb9
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-07-26 06:37:16 +0100

    databse config abd cors config

--- config/database.js
+++ config/database.js
@@ -34,25 +34,5 @@ module.exports = {
       acquire: 30000,
       idle: 10000
     }
-  },
-  qa2: {
-    HOST: "localhost",//"185.247.119.207",
-    USER: "qauser",
-    PASSWORD: "2SXzaQQu;",
-    DB: "everest_qa2",
-    dialect: "mysql",
-
-    dialectOptions: {
-      ssl: {
-        require: true,
-        ca: fs.readFileSync(__dirname + '/../server/ssl/crt.pem')
-      }
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
   }
 };
