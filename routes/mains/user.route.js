00b4b5f758a7554e958f0f0c170399c1d2585615
= <=>
2023-03-25 19:02:53 +0100

    user routes update

--- routes/mains/user.route.js
+++ routes/mains/user.route.js
@@ -3,14 +3,18 @@ var {asyncHandler} = require('../route')
 let controller = require('../../controllers/mains/user.controller');
 let UserHelper = require('../../application/mains/user.helper');
 
+let upload = require('../../application/upload/upload')
+
 // Export API routes
 module.exports = router;
 router.get('/',  controller.allBy);
 router.get('/:id',  controller.one);
+
 router.post('/',  asyncHandler(controller.create));
-router.post('/signon',  asyncHandler(controller.create), UserHelper.setUserAuthToken);
-router.put('/',  controller.update);
+router.put('/', upload.type("avatars").single("photoUrl"), controller.update);
+
 router.delete('/',  controller.delete);
+router.post('/signon',  asyncHandler(controller.create), UserHelper.setUserAuthToken);
 router.post('/authenticate', asyncHandler(controller.authenticate), UserHelper.setUserAuthToken);
 
 // Export API routes
