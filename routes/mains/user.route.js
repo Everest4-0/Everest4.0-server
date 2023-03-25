2e8a67eed3c866bd33c8b325cc3e38b399bb3efe
= <=>
2023-03-25 23:07:01 +0100

    multer removed

--- routes/mains/user.route.js
+++ routes/mains/user.route.js
@@ -3,15 +3,13 @@ var {asyncHandler} = require('../route')
 let controller = require('../../controllers/mains/user.controller');
 let UserHelper = require('../../application/mains/user.helper');
 
-let upload = require('../../application/upload/upload')
-
 // Export API routes
 module.exports = router;
 router.get('/',  controller.allBy);
 router.get('/:id',  controller.one);
 
 router.post('/',  asyncHandler(controller.create));
-router.put('/', upload.type("avatars").single("photoUrl"), controller.update);
+router.put('/',  controller.update);
 
 router.delete('/',  controller.delete);
 router.post('/signon',  asyncHandler(controller.create), UserHelper.setUserAuthToken);
